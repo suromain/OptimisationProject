0ea3d574475bff2bc95b9046d76ba7d8adbe48c8
Grégoire Bellon <gregoire.bellon@outlook.fr>
2024-01-15 11:19:52 +0100

    feat : utilisation de la bibliothèque python minizinc

--- flask/app/model/zebra/solver.py
+++ flask/app/model/zebra/solver.py
@@ -1,30 +1,8 @@
 import json
 import subprocess
 import tempfile
-
-
-def write_datafile(
-    tshirts: list[str],
-    names: list[str],
-    surnames: list[str],
-    pastas: list[str],
-    wines: list[str],
-    ages: list[int],
-) -> str:
-    tmp = tempfile.NamedTemporaryFile(delete=False, suffix=".dzn")
-    file_str = f"""
-    tshirt={str(tshirts)};
-    name={str(names)};
-    surname={str(surnames)};
-    pasta={str(pastas)};
-    wine={str(wines)};
-    age={str(ages)};
-    """
-    tmp.write(file_str.encode())
-
-    tmp.close()
-    return tmp.name
-
+import os
+from minizinc import Instance, Model, Solver
 
 def solve(
     tshirts: list[str],
@@ -34,7 +12,24 @@ def solve(
     wines: list[str],
     ages: list[int],
 ) -> list[bool]:
-    filename = write_datafile(tshirts, names, surnames, pastas, wines, ages)
-    command = f"minizinc app/model/zebra/model.mzn {filename} | tr -d '-'"
-    result = subprocess.check_output(command, shell=True, text=True)
-    return json.loads(result)
+
+    
+    print("solving")
+
+    gecode = Solver.lookup("gecode")
+
+    print(os.getcwd())
+    problem = Model("./app/model/zebra/model.mzn")
+
+    instance = Instance(gecode, problem)
+
+    instance["tshirt"] = tshirts
+    instance["name"] = names
+    instance["surname"] = surnames
+    instance["pasta"] = pastas
+    instance["wine"] = wines
+    instance["age"] = ages
+
+    result = instance.solve()
+
+    return result.solution.constraints
