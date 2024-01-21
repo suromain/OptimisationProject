945396147af6a98a756debf1947fe3475ed5dca3
Chaïmaâ Touhami <76920840+ChipChoped@users.noreply.github.com>
2024-01-21 20:59:34 +0100

    Fix model file not being found

--- flask/app/model/zebra/solver.py
+++ flask/app/model/zebra/solver.py
@@ -1,28 +1,22 @@
-import json
-import subprocess
-import tempfile
 import os
+
 from minizinc import Instance, Model, Solver
 
-def solve(
-    tshirts: list[str],
-    names: list[str],
-    surnames: list[str],
-    pastas: list[str],
-    wines: list[str],
-    ages: list[int],
-) -> list[bool]:
 
-    
-    print("solving")
+def solve(
+        tshirts: list[str],
+        names: list[str],
+        surnames: list[str],
+        pastas: list[str],
+        wines: list[str],
+        ages: list[int],
+) -> list[bool]:
+    print("Solving...")
 
     gecode = Solver.lookup("gecode")
-
-    print(os.getcwd())
-    problem = Model("./app/model/zebra/model.mzn")
+    problem = Model(os.getcwd() + "/flask/app/model/zebra/model.mzn")
 
     instance = Instance(gecode, problem)
-
     instance["tshirt"] = tshirts
     instance["name"] = names
     instance["surname"] = surnames
