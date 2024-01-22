4788d1f8641f59502cfd7b82ff436a7f31ac3007
Chaïmaâ Touhami <76920840+ChipChoped@users.noreply.github.com>
2024-01-22 01:16:45 +0100

    Web site paths

--- reactjs/app/src/main.tsx
+++ reactjs/app/src/main.tsx
@@ -2,9 +2,37 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import Error from "./components/error.tsx";
+import Home from "./components/home.tsx";
+import Zebra from "./components/zebra.tsx";
+import PersonalComputer from "./components/personal-computer.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    errorElement: <Error/>,
+    children: [
+      {
+        path:"/",
+        element:<Home/>
+      },
+      {
+        path:"/puzzle/zebra",
+        element:<Zebra/>
+      },
+      {
+        path:"/puzzle/personal-computer",
+        element:<PersonalComputer/>
+      }
+    ]
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
