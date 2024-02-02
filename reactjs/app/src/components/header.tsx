a479be1d5cf34fad689c0f3cd5cc2da1cdbf02f9
Chaïmaâ Touhami <76920840+ChipChoped@users.noreply.github.com>
2024-02-02 11:45:05 +0100

    Change header container

--- reactjs/app/src/components/header.tsx
+++ reactjs/app/src/components/header.tsx
@@ -1,33 +1,28 @@
-// import { Logo } from "./logo"
-// import "../styles/header.scss"
+import "../styles/header.css"
 
 import { Link } from "react-router-dom";
 
 export function Header() {
   return (
-    <>
+    <div className="menu">
       <div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/puzzle/zebra">Zebra Puzzle</Link>
-          </li>
-          <li>
-            <Link to="/puzzle/personal-computer">Personal Computer Puzzle</Link>
-          </li>
-          <li>
-            <Link to="/puzzle/movie-buffs">Movie Buffs Puzzle</Link>
-          </li>
-          <li>
-            <Link to="/custom/new">Create puzzle</Link>
-          </li>
-          <li>
-            <Link to="/custom">Custom puzzles</Link>
-          </li>
-        </ul>
+        <Link to="/">Home</Link>
       </div>
-    </>
-  );
+      <div>
+        <Link to="/puzzle/zebra">Zebra Puzzle</Link>
+      </div>
+      <div>
+        <Link to="/puzzle/personal-computer">Personal Computer Puzzle</Link>
+      </div>
+      <div>
+        <Link to="/puzzle/movie-buffs">Movie Buffs Puzzle</Link>
+      </div>
+      <div>
+        <Link to="/custom/new">Create puzzle</Link>
+      </div>
+      <div>
+        <Link to="/custom">Custom puzzles</Link>
+      </div>
+    </div>
+  )
 }
