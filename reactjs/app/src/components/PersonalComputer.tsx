ff0782ceff2047391c76a5b6ef77dad14feeee0f
Chaïmaâ Touhami <76920840+ChipChoped@users.noreply.github.com>
2024-02-02 16:45:31 +0100

    Answer array + hints + response popup

--- reactjs/app/src/components/PersonalComputer.tsx
+++ reactjs/app/src/components/PersonalComputer.tsx
@@ -1,19 +1,38 @@
-// import {useEffect, useState} from 'react';
-// import {useNavigate} from "react-router-dom";
-// import sdk from "../utils/sdk.ts";
-// import {PersonalComputerSolution} from "../types/PersonalComputerSolution.ts";
-
 import "../styles/grid.css"
+import "../styles/logicPuzzle.css"
 import tick from "../assets/tick.svg"
 import cross from "../assets/cross.svg"
 import {ReactElement, useEffect, useState} from "react";
 import {GridCellMatrix} from "../classes/GridCellMatrix.ts";
 import {CellState} from "../types/CellState.ts";
+import {HardDisks, Monitors, PersonalComputerSolution, Prices, Processors} from "../types/PersonalComputerSolution.ts";
+import sdk from "../utils/sdk.ts";
+import {useLoaderData} from "react-router-dom";
+import {ConstraintsResponse} from "../types/SolverResponse.ts";
 
 
 function PersonalComputer() {
   const height: number = 5;
   const width: number = 5;
+
+  const monitors: Array<Monitors> = [130, 150, 156, 215, 270];
+  const processors: Array<Processors> = [20, 23, 25, 27, 31];
+  const hardDisks: Array<HardDisks> = [250, 320, 500, 750, 1024];
+  const prices: Array<Prices> = [699, 999, 1149, 1349, 1649];
+
+  const monitorsLabels: Array<string> = ["13'", "15'", "15.6'", "21.5'", "27'"];
+  const processorsLabels: Array<string> = ["2.0 MHZ", "2.3 MHZ", "2.5 MHZ", "2.7 MHZ", "3.1 MHZ"];
+  const hardDisksLabels: Array<string> = ["250 Gb", "320 Gb", "500 Gb", "750 Gb", "1024 Gb"];
+  const pricesLabels: Array<string> = ["$ 699,00", "$ 999,00", "$ 1.149,00", "$ 1.349,00", "$ 1.649,00"];
+
+  const [answer, setAnswer] = useState<PersonalComputerSolution>((
+    {
+      monitors: monitors,
+      processors: new Array<Processors>(5).fill(null),
+      hardDisks: new Array<HardDisks>(5).fill(null),
+      prices: new Array<Prices>(5).fill(null)
+    }
+  ));
 
   // Index 0-2: first column, index 3-4: second column, index 6: third column
   const [puzzleGrid, setPuzzleGrid]
@@ -27,6 +46,34 @@ function PersonalComputer() {
   });
 
   const [overedCell, setOveredCell] = useState<Array<number>>([-1, -1, -1]);
+  const [checkAnswer, setCheckAnswer] = useState<boolean>(false);
+
+  const constraintsResponse: ConstraintsResponse = useLoaderData() as ConstraintsResponse;
+  const constraints = constraintsResponse.constraints;
+  const [constraintsOverlined, setConstraintsOverlined]
+    = useState<Array<boolean>>(new Array<boolean>(constraints.length).fill(false));
+
+  useEffect(() => {
+    if (checkAnswer) {
+      const api = sdk.personalComputer;
+      const response = api.checkSolution(answer);
+      response.then((result) => {
+        console.log(result)
+
+        if (result.every((value) => value.success)) {
+          window.confirm("Congratulations! You solved the puzzle!");
+          return;
+        }
+        else {
+          window.confirm("You made a mistake. Try again!");
+        }
+      }).catch((json) => {
+        console.log(json.response.status + " " + json.response.message)
+      }).finally(() => {
+        setCheckAnswer(false);
+      });
+    }
+  }, [answer, checkAnswer]);
 
   function rowCellOvered(matrixRow: number, labelRow: number): boolean {
     if (matrixRow === 0)
@@ -103,6 +150,27 @@ function PersonalComputer() {
       throw new Error("Invalid cell state");
   }
 
+  function updateAnswer(matrix: number, row: number, column: number, ticked: boolean) {
+    if (ticked) {
+      if (matrix == 0)
+        answer.processors[row] = processors[column];
+      else if (matrix == 3)
+        answer.hardDisks[row] = hardDisks[column];
+      else if (matrix == 5)
+        answer.prices[row] = prices[column];
+    }
+    else {
+      if (matrix == 0)
+        answer.processors[row] = null;
+      else if (matrix == 3)
+        answer.hardDisks[row] = null;
+      else if (matrix == 5)
+        answer.prices[row] = null;
+    }
+
+    setAnswer({...answer});
+  }
+
   function generateClickableMatrix(matrix: number): ReactElement {
     return (
       <tbody className="clickable">
@@ -116,8 +184,17 @@ function PersonalComputer() {
               columns.push(<td  key={j}
                 onPointerEnter={() => setOveredCell([matrix, i, j])}
                 onClick={() => {
+                  const wasTicked = puzzleGrid[matrix].getCellState(i, j) === CellState.TICKED;
+
                   puzzleGrid[matrix].changeCellState(i, j);
                   setPuzzleGrid([...puzzleGrid]);
+
+                  if (matrix === 0 || matrix === 3 || matrix === 5) {
+                    if (puzzleGrid[matrix].getCellState(i, j) === CellState.TICKED)
+                      updateAnswer(matrix, i, j, true);
+                    else if (wasTicked)
+                      updateAnswer(matrix, i, j, false)
+                  }
                 }}>
                 {getCellContent(matrix, i, j)}
               </td>);
@@ -153,36 +230,113 @@ function PersonalComputer() {
   }
 
   return (
-    <div style={{ display: "flex", justifyContent: "center" }}>
-      <div className="main-grid" onPointerLeave={() => setOveredCell([-1, -1, -1])}>
+    <div className="main-container">
+      <div className="body-container">
+        <div className="main-grid" onPointerLeave={() => setOveredCell([-1, -1, -1])}>
+          <table>
+            <tbody className="void" onPointerEnter={() => setOveredCell([-1, -1, -1])}/>
+            {generateLeftSide("Monitor", monitorsLabels, 0)}
+            {generateLeftSide("Price", pricesLabels, 1)}
+            {generateLeftSide("Hard Disk", hardDisksLabels, 2)}
+          </table>
+          <table>
+            {generateTopSide("Processor", processorsLabels, 3)}
+            {generateClickableMatrix(0)}
+            {generateClickableMatrix(1)}
+            {generateClickableMatrix(2)}
+          </table>
+          <table>
+            {generateTopSide("Hard Disk", hardDisksLabels, 4)}
+            {generateClickableMatrix(3)}
+            {generateClickableMatrix(4)}
+            {generateVoidMatrix()}
+          </table>
+          <table>
+            {generateTopSide("Price", pricesLabels, 5)}
+            {generateClickableMatrix(5)}
+            {generateVoidMatrix()}
+            {generateVoidMatrix()}
+          </table>
+        </div>
+
+        <div className="hints">
+          <div>
+            {constraints.map((constraint): ReactElement => {
+              const index = constraints.indexOf(constraint);
+              const overlined = constraintsOverlined[index];
+
+              if (!overlined)
+                return <p key={index} onClick={() => {
+                  constraintsOverlined[index] = true;
+                  setConstraintsOverlined([...constraintsOverlined]);
+                }}>{constraint}</p>;
+              else
+                return <></>
+            })}
+          </div>
+          <div>
+            {constraints.map((constraint): ReactElement => {
+              const index = constraints.indexOf(constraint);
+              const overlined = constraintsOverlined[index];
+
+              if (overlined)
+                return <p className="overlined" key={index} onClick={() => {
+                  constraintsOverlined[index] = false;
+                  setConstraintsOverlined([...constraintsOverlined]);
+                }}>{constraint}</p>;
+              else
+                return <></>
+            })}
+          </div>
+        </div>
+      </div>
+
+      <div className="answer-container">
         <table>
-          <tbody className="void" onPointerEnter={() => setOveredCell([-1, -1, -1])}/>
-          {generateLeftSide("Monitor", ["13'", "15'", "15.6'", "21.5'", "27'"], 0)}
-          {generateLeftSide("Price", ["$ 699,00", "$ 999,00", "$ 1.149,00", "$ 1.349,00", "$ 1.649,00"], 1)}
-          {generateLeftSide("Hard Disk", ["250 Gb", "320 Gb", "500 Gb", "750 Gb", "1024 Gb"], 2)}
+          <tbody>
+          <tr>
+            <th>Monitor</th>
+            <th>Processor</th>
+            <th>Hard Disk</th>
+            <th>Price</th>
+          </tr>
+          <tr>
+            <td>{monitorsLabels[monitors.indexOf(answer.monitors[0])]}</td>
+            <td>{processorsLabels[processors.indexOf(answer.processors[0])]}</td>
+            <td>{hardDisksLabels[hardDisks.indexOf(answer.hardDisks[0])]}</td>
+            <td>{pricesLabels[prices.indexOf(answer.prices[0])]}</td>
+          </tr>
+          <tr>
+            <td>{monitorsLabels[monitors.indexOf(answer.monitors[1])]}</td>
+            <td>{processorsLabels[processors.indexOf(answer.processors[1])]}</td>
+            <td>{hardDisksLabels[hardDisks.indexOf(answer.hardDisks[1])]}</td>
+            <td>{pricesLabels[prices.indexOf(answer.prices[1])]}</td>
+          </tr>
+          <tr>
+            <td>{monitorsLabels[monitors.indexOf(answer.monitors[2])]}</td>
+            <td>{processorsLabels[processors.indexOf(answer.processors[2])]}</td>
+            <td>{hardDisksLabels[hardDisks.indexOf(answer.hardDisks[2])]}</td>
+            <td>{pricesLabels[prices.indexOf(answer.prices[2])]}</td>
+          </tr>
+          <tr>
+            <td>{monitorsLabels[monitors.indexOf(answer.monitors[3])]}</td>
+            <td>{processorsLabels[processors.indexOf(answer.processors[3])]}</td>
+            <td>{hardDisksLabels[hardDisks.indexOf(answer.hardDisks[3])]}</td>
+            <td>{pricesLabels[prices.indexOf(answer.prices[3])]}</td>
+          </tr>
+          </tbody>
         </table>
-        <table>
-          {generateTopSide("Processor", ["2.0 MHZ", "2.3 MHZ", "2.5 MHZ", "2.7 MHZ", "3.1 MHZ"], 3)}
-          {generateClickableMatrix(0)}
-          {generateClickableMatrix(1)}
-          {generateClickableMatrix(2)}
-        </table>
-        <table>
-          {generateTopSide("Hard Disk", ["250 Gb", "320 Gb", "500 Gb", "750 Gb", "1024 Gb"], 4)}
-          {generateClickableMatrix(3)}
-          {generateClickableMatrix(4)}
-          {generateVoidMatrix()}
-        </table>
-        <table>
-          {generateTopSide("Price", ["$ 699,00", "$ 999,00", "$ 1.149,00", "$ 1.349,00", "$ 1.649,00"], 5)}
-          {generateClickableMatrix(5)}
-          {generateVoidMatrix()}
-          {generateVoidMatrix()}
-        </table>
+
+        <div className="button">
+          {answer.processors.includes(null) || answer.hardDisks.includes(null) || answer.prices.includes(null)
+            ? <button disabled>Check answer</button>
+            : <button onClick={() => {
+              setCheckAnswer(true);
+            }}>Check answer</button>}
+        </div>
       </div>
     </div>
   )
 }
 
 export default PersonalComputer;
-
