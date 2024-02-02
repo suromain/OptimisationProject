31db0918e2dce86796a74b18670f86813e62de36
Chaïmaâ Touhami <76920840+ChipChoped@users.noreply.github.com>
2024-02-02 16:43:44 +0100

    Update PersonalComputerSolution.ts

--- reactjs/app/src/types/PersonalComputerSolution.ts
+++ reactjs/app/src/types/PersonalComputerSolution.ts
@@ -1,7 +1,7 @@
-export type Processors = 25 | 20 | 23 | 27 | 31;
-export type Prices = 1349 | 999 | 699 | 1149 | 1649;
-export type Monitors = 156 | 130 | 150 | 215 | 270;
-export type HardDisks = 500 | 320 | 250 | 1024 | 750;
+export type Processors = 25 | 20 | 23 | 27 | 31 | null;
+export type Prices = 1349 | 999 | 699 | 1149 | 1649 | null;
+export type Monitors = 156 | 130 | 150 | 215 | 270 | null;
+export type HardDisks = 500 | 320 | 250 | 1024 | 750 | null;
 
 export interface PersonalComputerSolution {
   processors: Processors[];
