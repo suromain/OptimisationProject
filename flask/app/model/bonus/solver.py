8d1bc795f4c5906e40a223639fa7cde4e176310e
suromain <115778756+suromain@users.noreply.github.com>
2024-02-02 10:23:32 +0100

    Feat/variabiliser operands (#6)
    * feat : début variabilisation
    
    * fix : debug
    
    ---------
    
    Co-authored-by: Grégoire Bellon <gregoire.bellon@outlook.fr>

--- flask/app/model/bonus/solver.py
+++ flask/app/model/bonus/solver.py
@@ -1,12 +1,11 @@
 from minizinc import Instance, Model, Solver, Result, Status
-from app.validation.bonus import Constraint, Comparator, CustomAnswser
+from app.validation.bonus import Constraint, Comparator, CustomAnswer, CustomContent, OperandType
 from app.model.bonus.params import personnes, lieux, objets
 
 
-def create_and_solve(constraints : list[Constraint], answer : CustomAnswser) -> bool:
+def create_and_solve(content : CustomContent, answer : CustomAnswer) -> bool:
     
-
-    instance : Instance = create_instance(constraints)
+    instance : Instance = create_instance(content)
     
     instance["personnes"] = answer.personnes
     instance["lieux"] = answer.lieux
@@ -16,25 +15,37 @@ def create_and_solve(constraints : list[Constraint], answer : CustomAnswser) -> 
 
     return result.status == Status.SATISFIED
 
-def create_instance(constraints : list[Constraint]) -> Instance:
+def create_header(content : CustomContent) -> str:
+    header = "enum PERSONNE = { " + ', '.join([name for name in content.operands.names]) + "}; \n"
+    header += "enum LIEU = { " + ', '.join([lieu for lieu in content.operands.places]) + "}; \n"
+    header += "enum OBJET = { " + ', '.join([objet for objet in content.operands.objects]) + "}; \n"
+
+    header += "array[SOLUTIONS] of var PERSONNE: personnes;\n"
+    header += "array[SOLUTIONS] of var LIEU: lieux;\n"
+    header += "array[SOLUTIONS] of var OBJET: objets;\n"
+
+    header += "constraint all_different(personnes); \n"
+    header += "constraint all_different(lieux); \n"
+    header += "constraint all_different(objets); \n"
+
+    return header
+
+def create_instance(content : CustomContent) -> Instance:
     gecode = Solver.lookup("gecode")
     problem = Model("./app/model/bonus/model.mzn")
 
-    constraints_str = "\n".join([constraints_to_str(constraint) for constraint in constraints])
+    header = create_header(content)
+    constraints_str = "\n".join([constraints_to_str(constraint) for constraint in content.constraints])
 
     print(f"Using the constraints : \n{constraints_str}")
+    print(f"Using the header : \n{header}")
     
+    problem.add_string(header)
     problem.add_string(constraints_str)
 
     instance = Instance(gecode, problem)
 
     return instance
     
-
-def get_array_from_element(element : str) -> str:
-    if element in personnes: return "personnes" 
-    if element in lieux: return "lieux" 
-    return "objets" 
-
 def constraints_to_str(constraint : Constraint)-> str:
-    return f'constraint exists(sol in SOLUTIONS)({constraint.to_minizinc_constraint("sol", get_array_from_element)});'
+    return f'constraint exists(sol in SOLUTIONS)({constraint.to_minizinc_constraint("sol")});'
