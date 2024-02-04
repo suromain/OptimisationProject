8148c595bdc2aeb33e9be68b21f44f145c478a7a
Chaïmaâ Touhami <76920840+ChipChoped@users.noreply.github.com>
2024-02-04 13:59:59 +0100

    Add footer

--- reactjs/app/src/App.tsx
+++ reactjs/app/src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
 import {Header} from "./components/Header.tsx";
 import {Outlet} from "react-router-dom";
+import {Footer} from "./components/Footer.tsx";
 
 function App() {
   return (
     <>
       <Header />
       <Outlet />
+      <Footer />
     </>
   )
 }
